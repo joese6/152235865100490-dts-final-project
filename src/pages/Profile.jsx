56a4f8daa475787e3b4bd33692c0dd7d0d2d8974
Johan <joese6@gmail.com>
2022-08-02 10:36:18 +0700

    feat: final project

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -9,6 +9,7 @@ export default function Profile(){
             <h1 className="section-title">Profile saya</h1>
             <p className="section-description">Saya adalah junior web developer basis ReactJS</p>
             <p className="section-description">Nama : Zohan Fachrullwahab</p>
+            <p className="section-description">No Registrasi : 152235865100-490</p>
         </section>
         )
 }
